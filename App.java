e3691df8f6509d24fefa0fe6c7c339fee57cc6fc
hsinjlee <hlee294@asu.edu>
2020-07-21 12:45:47 -0700

    add Iterative Deepening Search

--- App.java
+++ App.java
@@ -1,23 +1,28 @@
-package BreadthFirstSearch;
+package IterativeDeepeningSearch;
 
 public class App {
-	public static void main(String[] args) {
-		BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch();
-		Vertex vertex1 = new Vertex(1);
-		Vertex vertex2 = new Vertex(2);
-		Vertex vertex3 = new Vertex(3);
-		Vertex vertex4 = new Vertex(4);
-		Vertex vertex5 = new Vertex(5);
-		Vertex vertex6 = new Vertex(6);
-		Vertex vertex7 = new Vertex(7);
-		
-		vertex1.addNeighbour(vertex2);
-		vertex1.addNeighbour(vertex4);
-		vertex4.addNeighbour(vertex5);
-		vertex2.addNeighbour(vertex3);
-		vertex5.addNeighbour(vertex7);
-		vertex4.addNeighbour(vertex6);
-		
-		breadthFirstSearch.bfs(vertex1);
-	}
+
+    public static void main(String[] args) {
+
+        Node vertex0 = new Node("A");
+        Node vertex1 = new Node("B");
+        Node vertex2 = new Node("C");
+        Node vertex3 = new Node("D");
+        Node vertex4 = new Node("E");
+
+        vertex0.addNeighbour(vertex1);
+        vertex0.addNeighbour(vertex2);
+        vertex1.addNeighbour(vertex3);
+        vertex3.addNeighbour(vertex4);
+
+        Algorithm algorithm = new Algorithm(vertex4);
+        algorithm.runDeepeningSearch(vertex0);
+
+    }
 }
+
+//        A
+//        A C B
+//        A C B D
+//        A C B D E
+//        Node found...
